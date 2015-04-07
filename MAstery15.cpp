f59af78ea154fbf8b4556512043fa90353e20bbd
Monserrat <a01630174@itesm.mx>
2015-04-06 19:06:35 -0500

    Create MAstery15.cpp

---
+++ MAstery15.cpp
@@ -0,0 +1,21 @@
+//Mastery15
+//TC1017
+// Uso de IF
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	
+	cout << "¿Cual es el monto de tu compra?" << endl;
+	cin >> a;
+	if (a>=200){
+
+		cout << "Tiene un bono del 10 pociento de su compra en su siguiente visita" << endl;
+	}
+	 return 0;
+	 
+
+}
+
