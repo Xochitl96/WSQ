6ecd4127ff200b6d5fcd20aeb8a1de3d1f89bec9
Monserrat <a01630174@itesm.mx>
2015-04-08 21:57:45 -0500

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,48 @@
+//WSQ14
+//TC1017
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int factorial(int n){
+	if (n==0){
+		return 1;
+	}
+	if (n>0) {
+		int fact=1;
+		for (int i=2;i<=n;i++){
+			fact=fact*i;
+		}
+		return fact;
+	}
+}
+
+double floor(double e, int precision){
+  e *= pow(10, precision+1);
+  e = floor(e);
+  e /= pow(10, precision+1);
+  return e;
+}
+
+float calculate_e(int precision){
+	float e = 0.0;
+	float prevguess;
+	for(float n = 0.0 ;n < 100; n++){
+		prevguess = e;
+		cout << n << "   " << prevguess << endl;
+		e += 1 / float (factorial(n));
+		if (floor(prevguess, precision) == floor(e, precision)){
+			break;
+		}
+	}
+	return e;
+}
+
+int main(){
+	int precision;
+	cout<<"Dame el numero que deseas calcular" <<endl;
+	cin >> precision;
+	cout << endl << calculate_e(precision) << endl;
+	return 0;
+}
