73b5cc1eabe0cb73c848b754c3ebdaf92609b461
Monserrat <a01630174@itesm.mx>
2015-05-06 17:16:13 -0500

    Create Mastery18.cpp

---
+++ Mastery18.cpp
@@ -0,0 +1,29 @@
+#Mastery18
+#Tc1017
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int promedio;
+
+	cout<< "Ingresa tu promedio general" <<endl;
+	cin>> promedio;
+
+	if(promedio>80){
+
+		cout<< "Felicidades, sigue esforzandote" <<endl;
+	}
+
+	else {
+		if (promedio<80)
+		{
+			cout << "Estas en la categoria de alumno irregular, mejora tu rendimiento academico"  <<endl;
+		}
+		else 
+		{
+			cout <<"Eres alumno regular, tu puedes mas " <<endl;
+		}
+	}
+	return 0;
+}
